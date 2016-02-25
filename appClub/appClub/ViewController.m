2fc28369fbb3d8f8781e06dc9513aa11d3bcbbbc
Madelyn <mnelso12@nd.edu>
2016-02-24 19:07:39 -0500

    Initial Commit

---
+++ appClub/appClub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  appClub
+//
+//  Created by MadelynNelson on 2/24/16.
+//  Copyright (c) 2016 Madelyn Nelson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
