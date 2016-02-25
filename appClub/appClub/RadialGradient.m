2a3dd267725cb690394546ee71aa15e6c741847e
Buck <bbourdon@nd.edu>
2016-02-24 19:32:10 -0500

    Pretty view effect

---
+++ appClub/appClub/RadialGradient.m
@@ -0,0 +1,41 @@
+//
+//  RadialGradient.m
+//  appClub
+//
+//  Created by Scott Bourdon on 2/24/16.
+//  Copyright © 2016 ND App Club. All rights reserved.
+//
+
+#import "RadialGradient.h"
+
+@implementation RadialGradient
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        [self setNeedsDisplay];
+    }
+    return self;
+}
+
+- (void)drawInContext:(CGContextRef)ctx
+{
+    
+    size_t gradLocationsNum = 2;
+    CGFloat gradLocations[2] = {0.0f, 1.0f};
+    CGFloat gradColors[8] = {0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.0f,0.5f};
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorSpace, gradColors, gradLocations, gradLocationsNum);
+    CGColorSpaceRelease(colorSpace);
+    
+    CGPoint gradCenter= CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+    float gradRadius = MIN(self.bounds.size.width , self.bounds.size.height) ;
+    
+    CGContextDrawRadialGradient (ctx, gradient, gradCenter, 0, gradCenter, gradRadius, kCGGradientDrawsAfterEndLocation);
+    
+    
+    CGGradientRelease(gradient);
+}
+
+@end
